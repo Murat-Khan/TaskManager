153e0b03cb3c58190928d5f3cfd4b3574c399588
Murat-Khan <kayfff995@gmail.com>
2022-11-21 12:05:39 +0600

    add shared pref

--- app/src/main/java/com/murat/taskmanager/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/murat/taskmanager/ui/profile/ProfileFragment.kt
@@ -1,59 +1,105 @@
 package com.murat.taskmanager.ui.profile
 
-import android.app.Activity
-import android.content.Intent
+
+
+
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
+import com.murat.taskmanager.data.local.Pref
 import com.murat.taskmanager.databinding.FragmentProfileBinding
+import com.murat.taskmanager.loadImage
+
 
 
 class ProfileFragment : Fragment() {
+    lateinit var pref: Pref
+
+
 
     private lateinit var binding: FragmentProfileBinding
-    companion object{
-        const val REQUEST_CODE = 55
-    }
 
+
+
+
+    private var galleryActivityLauncher: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.OpenDocument()
+        ) { result ->
+            if (result != null) {
+                binding.profileImage.loadImage(result.toString())
+                pref.saveImage(result.toString())
+            } else {
+                Log.d("lol", "onActivityResult: the result is null for some reason")
+            }
+        }
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentProfileBinding.inflate(inflater,container,false)
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
         return binding.root
+
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.profileImage.setOnClickListener{
-            openGalleryForImage()
-        }
 
 
-    }
-
-    private fun openGalleryForImage() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
-            binding.profileImage.setImageURI(data?.data)
+        binding.profileImage.setOnClickListener {
+            galleryActivityLauncher.launch(arrayOf("image/*"))
 
         }
-    }
+        saveProfileData()
 
 
     }
+
+
+
+    private fun saveProfileData() {
+        pref = Pref(requireContext())
+        binding.etName.setText(pref.getName())
+        binding.etName.addTextChangedListener {
+            pref.saveName(binding.etName.text.toString())
+        }
+        binding.etAge.setText(pref.getAge())
+        binding.etAge.addTextChangedListener {
+            pref.saveAge(binding.etAge.text.toString())
+        }
+
+        binding.profileImage.loadImage(pref.getImage())
+
+
+
+
+    }
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
